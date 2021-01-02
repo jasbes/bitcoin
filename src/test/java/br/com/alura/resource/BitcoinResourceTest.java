5353f84d4ca778cc89304197e663262a142708e1
Jefferson <jefferson_uk@hotmail.com>
2021-01-02 03:28:20 +0100

    - Test endpoint with restassured

--- src/test/java/br/com/alura/resource/BitcoinResourceTest.java
+++ src/test/java/br/com/alura/resource/BitcoinResourceTest.java
@@ -4,18 +4,16 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
-public class BitcoinResourceTest {
+class BitcoinResourceTest {
 
     @Test
-    public void testHelloEndpoint() {
-        given()
-          .when().get("/hello-resteasy")
-          .then()
-             .statusCode(200)
-             .body(is("Hello RESTEasy"));
-    }
+    void listar() {
 
+        given()
+                .when().get("/bitcoins")
+                .then()
+                .statusCode(200);
+    }
 }
--- src/test/java/br/com/alura/resource/NativeBitcoinResourceIT.java
+++
@@ -1,9 +0,0 @@
-package br.com.alura.resource;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeBitcoinResourceIT extends BitcoinResourceTest {
-
-    // Execute the same tests but in native mode.
-}
